02e09f0e45df850bf5e32fa0594372d393261ded
Adrien DA SILVA FERNANDES <adda-sil@e2r9p4.42.fr>
2019-10-23 19:25:16 +0200

    Remove libft

--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,22 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 20:20:55 by adda-sil          #+#    #+#             */
-/*   Updated: 2019/10/20 21:11:37 by adda-sil         ###   ########.fr       */
+/*   Updated: 2019/10/21 20:41:42 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int		ft_printf(const char *str, ...)
 {
 	va_list args;
 
 	va_start(args, str);
+	printf("str: %s\n", str);
+	printf("el: %d\n", va_arg(args, int));
 	va_end(args);
 	return (1);
 }
--- main.c
+++ main.c
@@ -1,25 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/10 17:35:59 by adda-sil          #+#    #+#             */
-/*   Updated: 2019/10/10 17:36:24 by adda-sil         ###   ########.fr       */
+/*   Created: 2019/10/21 19:41:54 by adda-sil          #+#    #+#             */
+/*   Updated: 2019/10/23 19:21:24 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "ft_printf.h"
+#include <stdio.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+int main(int ac, char **av)
 {
-	unsigned char		*tmp_dst;
-	const unsigned char *tmp_src;
-
-	tmp_dst = dst;
-	tmp_src = src;
-	while (n-- > 0)
-		*tmp_dst++ = *tmp_src++;
-	return (dst);
+    (void)ac;
+    (void)av;
+    ft_printf("hello", 42);
 }

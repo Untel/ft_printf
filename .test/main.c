5a805129df592eaf83cb65d920f468fd57c45289
Adrien DA SILVA FERNANDES <adda-sil@e1r11p8.42.fr>
2019-10-29 17:43:58 +0100

    Print base string

--- .test/main.c
+++ .test/main.c
@@ -6,7 +6,7 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 19:41:54 by adda-sil          #+#    #+#             */
-/*   Updated: 2019/10/29 16:31:39 by adda-sil         ###   ########.fr       */
+/*   Updated: 2019/10/29 16:38:06 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int ac, char **av)
 {
     (void)av;
     (void)ac;
- 
+
     PRINT("Hi %d", 42);
     PRINT("Bonjour %d", 42);
     PRINT("Yop %d", 42);
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 20:20:55 by adda-sil          #+#    #+#             */
-/*   Updated: 2019/10/28 20:54:50 by adda-sil         ###   ########.fr       */
+/*   Updated: 2019/10/29 17:42:54 by adda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,50 @@
  * Modifiers_bonus: "l ll h hh ’# +"
  */
 
-int		ft_printf(const char *str, ...)
+int		extract_flags(char *str)
 {
-	int 	i;
+	
+}
+
+int		split_to_list(const char *str, t_list **lst)
+{
+	int		i;
 	int		j;
 	char	*ptr;
-	i = -1;
+
+	i = 0;
 	j = 0;
-	printf("%s", str);
-	if (str[0] == 'H')
-		printf(" -yo");
-	while (str[++i])
+	while (str[i])
 	{
 		if (str[i] == '%')
 		{
-			ptr = ft_substr(str, j, (i - j));
-			j = i;
+			if (!(ptr = ft_substr(str, j, (i - j))))
+				return (0);
+			ft_lstadd_back(lst, ft_lstnew((char *)ptr));
+			i = extract_flags(&ptr[i]);
 		}
+		i++;
+	}
+	if (!(ptr = ft_substr(str, j, (i - j))))
+		return (0);
+	ft_lstadd_back(lst, ft_lstnew((char *)ptr));
+	convert_data(&ptr[i]);
+	return (1);
+}
+
+int		ft_printf(const char *str, ...)
+{
+	t_list	*el;
+	size_t	len;
+
+	len = 0;
+	split_to_list(str, &el);
+	while (el)
+	{
+		len += ft_strlen(el->content);
+		ft_putstr_fd((char *)el->content, 0);
+		printf((char *)el->content, 1);
+		el = el->next;
 	}
 	return (1);
 }
